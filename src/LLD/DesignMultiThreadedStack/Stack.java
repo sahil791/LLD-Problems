c30dc10db60aba56caa43b0b18b1fd5ce3d5da56
Sahil Bhandari <sahilbhandari@Sahils-MacBook-Pro.local>
2024-10-02 22:21:48 +0530

    fixing typo in stack interface

--- src/LLD/DesignMultiThreadedStack/Stack.java
+++ src/LLD/DesignMultiThreadedStack/Stack.java
@@ -3,6 +3,6 @@ package LLD.DesignMultiThreadedStack;
 public interface Stack<T> {
     void push(T val);
     T pop();
-    // totoal number of push|pop operations performed
+    // total number of push|pop operations performed
     long counter();
 }
